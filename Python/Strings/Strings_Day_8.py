00f8c9983783d63eb62a897c260e371c2c31677b
thandrasatish <61319878+thandrasatish@users.noreply.github.com>
2024-12-03 11:19:22 +0000

    Creating Strings Folder Day 8

---
+++ Python/Strings/Strings_Day_8.py
@@ -0,0 +1,11 @@
+# Anagram
+# Difficulty: EasyAccuracy: 44.93%Submissions: 362K+Points: 2
+# Given two strings s1 and s2 consisting of lowercase characters. The task is to check whether two given strings are an anagram of each other or not. An anagram of a string is another string that contains the same characters, only the order of characters can be different. For example, act and tac are an anagram of each other. Strings s1 and s2 can only contain lowercase alphabets.
+
+# Note: You can assume both the strings s1 & s2 are non-empty.
+
+# Examples :
+
+# Input: s1 = "geeks", s2 = "kseeg"
+# Output: true
+# Explanation: Both the string have same characters with same frequency. So, they are anagrams.
