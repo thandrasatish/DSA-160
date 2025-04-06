c866329cc364b710b8002a6e0c791bb542abc40e
thandrasatish <61319878+thandrasatish@users.noreply.github.com>
2025-04-06 23:03:36 +0000

    Longest Sub array Problem

--- prefixSum/LongestSubarrayWithSum.py
+++ prefixSum/LongestSubarrayWithSum.py
@@ -1,40 +1,32 @@
-# Longest SubArray with Sum K 
-# Difficulty: Medium
-# Given an array arr[] containing integers and an integer k, your task is to find the length of the longest subarray where the sum of its elements is equal to the given value k. If there is no subarray with sum equal to k, return 0.
-
-
-# Input: arr[] = [10, 5, 2, 7, 1, -10], k = 15
-# Output: 6
-# Explanation: Subarrays with sum = 15 are [5, 2, 7, 1], [10, 5] and [10, 5, 2, 7, 1, -10]. The length of the longest subarray with a sum of 15 is 6.
-# Input: arr[] = [-5, 8, -14, 2, 4, 12], k = -5
-# Output: 5
-# Explanation: Only subarray with sum = -5 is [-5, 8, -14, 2, 4] of length 5.
-# Input: arr[] = [10, -10, 20, 30], k = 5
-# Output: 0
-# Explanation: No subarray with sum = 5 is present in arr[].
-
 def longestSubarrayWithSumK(arr, k):
     n = len(arr)
     prefix_sum = 0 
     d = {}
     ans = 0
+    
+    # Iterate through the array
     for i in range(n):
+        # Update the prefix sum
         prefix_sum += arr[i]
         
+        # Check if the prefix sum is equal to the target sum k
+        # If true, the subarray from the beginning to the current index has the sum k
         if prefix_sum == k:
             ans = max(ans, i + 1)
         
+        # Check if there is any prefix sum that when subtracted from the current prefix sum gives k
+        # If true, it means there exists a subarray with sum k ending at the current index
         if (prefix_sum - k) in d:
             ans = max(ans, i - d[prefix_sum - k])
         
+        # Store the first occurrence of the prefix sum in the dictionary
+        # This helps in identifying the longest subarray with the desired sum
         if prefix_sum not in d:
             d[prefix_sum] = i
+    
     return ans
 
-
-
-
-
+# Test cases
 arr = [10, 5, 2, 7, 1, -10]
 k = 15
 print(longestSubarrayWithSumK(arr, k))  # Output: 6
